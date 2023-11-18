64ea9111916f44cb2da054050a02c606ba3ab8e0
Valery1524 <valerykarolina55@gmail.com>
2023-11-18 10:39:13 -0500

    Mant-Empleado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,1 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { VentaComponent } from './components/venta/venta.component';
+import { ReporteComponent } from './components/reporte/reporte.component';
+import { MantEmpleadoComponent } from './components/mant-empleado/mant-empleado.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarroComponent } from './components/carro/carro.component';
+import { AgregarComponent } from './components/carro/agregar/agregar.component'
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    VentaComponent,
+    ReporteComponent,
+    MantEmpleadoComponent,
+    NavbarComponent,
+    CarroComponent,
+    AgregarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
